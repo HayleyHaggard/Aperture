fe225f7ed0df9baa12acef01b9066926a210762d
HayleyHaggard <nastena_panova2000@mail.ru>
2022-07-13 17:43:13 +0300

    project is ready

---
+++ docs/js/main.js
@@ -0,0 +1,29 @@
+$(function () {
+
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('active');
+    $('.menu__list-link').toggleClass('animation');
+    document.querySelector('body').classList.toggle('noscroll');
+  });
+
+  AOS.init({
+    disable: function () {
+      let maxWidth = 1200;
+      return window.innerWidth < maxWidth;
+    },
+    startEvent: 'DOMContentLoaded',
+    offset: 100,
+    delay: 200,
+    duration: 1500,
+  });
+
+  window.onscroll = function () { scrollFunction() };
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      document.querySelector('header').classList.add('header-mini');
+    } else {
+      document.querySelector('header').classList.remove('header-mini');
+    }
+  }
+});
